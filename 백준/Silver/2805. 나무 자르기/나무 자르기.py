c965ba970f791704c6538c12c02d76abac6e95e6
SangHoon Kang <53715807+totohoon02@users.noreply.github.com>
2025-03-24 19:58:50 +0900

    [Silver II] Title: 나무 자르기, Time: 2792 ms, Memory: 143532 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,46 +1,40 @@
 import sys
-from typing import *
 
 input = sys.stdin.readline
 
 
-def readline_as_str_list(sep=" ") -> List[str]:
-    return input().rstrip().split(sep)
+def iinput():
+    return int(input().strip())
 
 
-def join(str_list: List[str], sep="") -> str:
-    return sep.join(str_list)
+def spt(sep=" "):
+    return input().strip().split(sep)
 
 
-def map_to_int_list(str_list: List[str]) -> List[int]:
-    return list(map(int, str_list))
+def spt_map_int(sep=" "):
+    return list(map(int, input().strip().split(sep)))
 
 
-class Solution:
-    def get_cutter_height(self):
-        # n: 나무 수, m: 가져갈 나무 길이
-        n, m = map_to_int_list(readline_as_str_list())
-        # 나무의 높이
-        heights = map_to_int_list(readline_as_str_list())
+def sol():
+    N, M = spt_map_int()
+    trees = spt_map_int()
 
-        start = 1
-        end = max(heights)
-        mid = 0
+    start = 1
+    end = max(trees)
+    mid = 0
 
-        while start <= end:
-            sum = 0
+    while start <= end:
+        total = 0
+        mid = (start + end) // 2
 
-            mid = (start + end) // 2
-            for h in heights:
-                if h >= mid:
-                    sum += h - mid
+        for tree in trees:
+            total += max(tree - mid, 0)
+        
+        if total >= M:
+            start = mid + 1
+        else:
+            end = mid - 1
+    
+    print(end)
 
-            if sum >= m:
-                start = mid + 1
-            else:
-                end = mid - 1
-        print(end)
-
-
-sol = Solution()
-sol.get_cutter_height()
+sol()
